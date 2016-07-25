ae58daf1601fc608140475334358ba258513bdea
Technius <techniux@gmail.com>
2016-07-24 22:26:47 -0700

    Install Scala and SBT

--- setup_pkgs.sh
+++ setup_pkgs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Installs packages; assumes root
 
 PKGS=$(cat debian-pkgs | sed -re 's/#.*//g' -e '/^\s*$/d')
 
@@ -30,8 +31,30 @@ install_ppa_pkg_if_needed() {
 apt-get update
 apt-get install $PKGS
 
+# This is needed to install from https sources
+install_if_needed apt-transport-https
+
 # PPAs
 install_if_needed software-properties-common
 
 # Lollypop music player
 install_ppa_pkg_if_needed gnumdk/lollypop lollypop
+
+# Scala
+if ! is_installed scala; then
+    scala_ver="2.11.8"
+    wget http://downloads.lightbend.com/scala/$scala_ver/scala-$scala_ver.deb \
+        -O /tmp/scala-$scala_ver.deb
+    dpkg -i /tmp/scala-$scala_ver.deb
+else
+    echo "Scala is already installed, skipping"
+fi
+
+# SBT
+if ! is_installed sbt; then
+    echo "deb https://dl.bintray.com/sbt/debian /" > /etc/apt/sources.list.d/sbt.list
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
+    apt-get update && apt-get install sbt
+else
+    echo "SBT is already installed, skipping"
+fi
