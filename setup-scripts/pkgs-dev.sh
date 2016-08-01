347f42a9c107a742c921e0d2628652ed0b57b8ab
Technius <techniux@gmail.com>
2016-08-01 12:13:42 -0700

    Add missing sudo to pkgs-dev.sh

--- setup-scripts/pkgs-dev.sh
+++ setup-scripts/pkgs-dev.sh
@@ -25,13 +25,13 @@ fi
 if prompt_install "Scala"; then
     wget http://downloads.lightbend.com/scala/$scala_ver/scala-$scala_ver.deb \
         -O /tmp/scala-$scala_ver.deb
-    dpkg -i /tmp/scala-$scala_ver.deb
+    sudo dpkg -i /tmp/scala-$scala_ver.deb
 fi
 
 if prompt_install "Scala SBT"; then
     echo "deb https://dl.bintray.com/sbt/debian /" > /etc/apt/sources.list.d/sbt.list
     sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
-    apt-get update && apt-get install sbt
+    sudo apt-get update && apt-get install sbt
 fi
 
 prompt_install "Haskell Stack" "which stack" && \
